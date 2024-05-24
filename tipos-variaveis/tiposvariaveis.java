7be180705ffd571a4b9426a787d4527676b5e8d8
GiovaniMoura <thegiovanimoura@gmail.com>
2024-05-23 22:27:22 -0300

    Aulas de Sintaxe e estruturas condicionais

---
+++ tipos-variaveis/tiposvariaveis.java
@@ -0,0 +1,18 @@
+public class tiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinino = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+        
+
+
+    }
+}
